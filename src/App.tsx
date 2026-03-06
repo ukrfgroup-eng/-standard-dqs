a5d8348042cba79c2f931bc6833b679c10455946
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-03-06 13:06:46 +0300

    Add Hero and Advantages components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-// Файл src/App.tsx
-import { Header } from './blocks/Header/Header'; // <-- Указали путь к файлу внутри папки
+import React from 'react';
+import { Header } from './blocks/Header/Header';
+import { Hero } from './blocks/Hero/Hero';
+import { Advantages } from './blocks/Advantages/Advantages';
 
 export default function App() {
   return (
     <main className="bg-[#06060a] min-h-screen text-[#f0eee8]">
       <Header />
+      <Hero />
+      <Advantages />
     </main>
   );
 }
