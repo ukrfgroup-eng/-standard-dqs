5df81307a71d095a58389dc3ceb687a7297f46ae
ukrfgroup-eng <uk.rfgroup@gmail.com>
2026-03-06 13:19:54 +0300

    Add Stats component to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Header } from './blocks/Header/Header';
 import { Hero } from './blocks/Hero/Hero';
 import { Advantages } from './blocks/Advantages/Advantages';
 import { Showcase } from './blocks/Showcase/Showcase';
+import { Stats } from './blocks/Stats/Stats';
 
 export default function App() {
   return (
     <main className="bg-[#06060a] min-h-screen text-[#f0eee8]">
       <Header />
       <Hero />
+      <Stats />
       <Advantages />
       <Showcase />
     </main>
